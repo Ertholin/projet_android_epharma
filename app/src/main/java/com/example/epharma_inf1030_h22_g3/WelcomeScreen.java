45524d5dccc952e2f343f10e5c63044405fe463a
Ertholin <ertholinulysse@gmail.com>
2022-03-25 15:48:26 -0400

    Premier commit de l'einterface

---
+++ app/src/main/java/com/example/epharma_inf1030_h22_g3/WelcomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.epharma_inf1030_h22_g3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
