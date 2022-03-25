b1e4f80f889c776148f383440e4700ebfdb07f53
Ertholin <ertholinulysse@gmail.com>
2022-03-25 19:46:17 -0400

    Ajout du Fragment Login au Item du menu Profile

--- app/src/main/java/com/example/epharma_inf1030_h22_g3/MainActivity.java
+++ app/src/main/java/com/example/epharma_inf1030_h22_g3/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.epharma_inf1030_h22_g3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.epharma_inf1030_h22_g3.databinding.ActivityMainBinding;
+import com.example.epharma_inf1030_h22_g3.tab.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binder = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(binder.getRoot());
+        binder.appMenu.setOnItemReselectedListener(this::menuClicked);
+    }
+
+    private boolean menuClicked(MenuItem menuItem){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        switch (menuItem.getItemId()){
+            case R.id.menu_profile:
+                LoginFragment lf = new LoginFragment();
+                transaction.replace(R.id.flFragment, lf, "Login");
+                transaction.commit();
+                break;
+        }
+
+        return false;
     }
 }
