bb9d79b386f12d4f4c4ae5824dca322cf88efb2b
Ertholin <ertholinulysse@gmail.com>
2022-03-26 20:31:04 -0400

    Personnaliser le fragment_item.xml

---
+++ app/src/main/java/com/example/epharma_inf1030_h22_g3/models/Auditable.java
@@ -0,0 +1,52 @@
+package com.example.epharma_inf1030_h22_g3.models;
+
+import androidx.annotation.NonNull;
+
+import java.time.LocalDateTime;
+
+public class Auditable {
+
+    @NonNull
+    private LocalDateTime created;
+    @NonNull
+    private LocalDateTime updated;
+
+    private boolean needUpdate;
+    private boolean deleted;
+
+    public Auditable(){
+        created = LocalDateTime.now();
+        updated = LocalDateTime.now();
+    }
+    public @NonNull LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(@NonNull LocalDateTime created) {
+        this.created = created;
+    }
+
+    public @NonNull LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(@NonNull LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public boolean isNeedUpdate() {
+        return needUpdate;
+    }
+
+    public void setNeedUpdate(boolean needUpdate) {
+        this.needUpdate = needUpdate;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+}
---
+++ app/src/main/java/com/example/epharma_inf1030_h22_g3/models/Medicament.java
@@ -0,0 +1,150 @@
+package com.example.epharma_inf1030_h22_g3.models;
+
+import androidx.room.Entity;
+
+@Entity()
+public class Medicament extends Auditable{
+
+    private Long catnumber;
+    private Long drugnumber;
+    private String dci;
+    private String dosage;
+    private String form;
+    private String reference;
+    private boolean cases;
+    private boolean posts;
+    private boolean centers;
+    private boolean eps1;
+    private boolean eps2;
+    private boolean eps3;
+    private String createdBy;
+    private String lastUpdatedBy;
+    private boolean status;
+
+    public Medicament(String dci, String dosage, String form, String reference) {
+        this.dci = dci;
+        this.dosage = dosage;
+        this.form = form;
+        this.reference = reference;
+    }
+
+    public Long getCatnumber() {
+        return catnumber;
+    }
+
+    public void setCatnumber(Long catnumber) {
+        this.catnumber = catnumber;
+    }
+
+    public Long getDrugnumber() {
+        return drugnumber;
+    }
+
+    public void setDrugnumber(Long drugnumber) {
+        this.drugnumber = drugnumber;
+    }
+
+    public String getDci() {
+        return dci;
+    }
+
+    public void setDci(String dci) {
+        this.dci = dci;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public void setForm(String form) {
+        this.form = form;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public boolean isCases() {
+        return cases;
+    }
+
+    public void setCases(boolean cases) {
+        this.cases = cases;
+    }
+
+    public boolean isPosts() {
+        return posts;
+    }
+
+    public void setPosts(boolean posts) {
+        this.posts = posts;
+    }
+
+    public boolean isCenters() {
+        return centers;
+    }
+
+    public void setCenters(boolean centers) {
+        this.centers = centers;
+    }
+
+    public boolean isEps1() {
+        return eps1;
+    }
+
+    public void setEps1(boolean eps1) {
+        this.eps1 = eps1;
+    }
+
+    public boolean isEps2() {
+        return eps2;
+    }
+
+    public void setEps2(boolean eps2) {
+        this.eps2 = eps2;
+    }
+
+    public boolean isEps3() {
+        return eps3;
+    }
+
+    public void setEps3(boolean eps3) {
+        this.eps3 = eps3;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
